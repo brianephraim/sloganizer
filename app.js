0418a9b75e247a052316f4afc87cb984e06a76f5
Brian Ephraim <brianephraim@gmail.com>
2013-06-24 19:20:57 -0400

    Sloganizer
    A widget for generating random sentences with cool
    mobile-device-capable animation.

---
+++ app.js
@@ -0,0 +1,26 @@
+var mySloganizer;
+$(function() {
+	var $sloganizerWrapper = $('.mySloganizerWrapper');
+	mySloganizer = Sloganizer.createSloganizer({
+		$el: $sloganizerWrapper.find('.sloganizer'),
+		wheelWordCount:3,
+		wordBanks: [
+			['Boomer','Behemoth','Kriksis'],
+			['is','isn\'t'],
+			['very','extremely','persistantly'],
+			['good.']
+			/*
+			['Hangout with','Join','Form','Summon','Interact with','Participate in','Make','Create','Keep','Release','Detect','Help','Share','Team up with','Build','Improve','Embrace','Improve','Power','Bestow','Invest in','Play with','Become','Change','Talk about','Understand','Learn about','Grow','Blab about','Have','Develop','Enjoy','Socialize'],
+			['friends','a friend','friendship','new people','blaboid','the mystery','the fun','some fun','others','another','robots','mobs','me','yourself','them','us','robots','activism','others','truck wine','myself','happiness','gatherings','togetherness','groups','individuality','robots','your interests','power','community','books','movies','shows','conversations','new friends','your cat','something new','new things','philsophy','culture','a sasquatcht','nanotubes','revolution','evolution','progress','politics','science','reddit','twitter','facebook','meetups','4square','cuttlefish','magic','numbers','sophistication','X-men','your cat','experiences','neighbors','articles','small groups'],
+			['with others.','mathmatically','metaphysically.','in reality.','with happiness.','robotically.','technologically.','radically.','greener.','substantially.','potentially.','and another.','randomly.','nearby.','locally.','powerfully.','communally.','with precision.','wholeheartidly.','with interest.','anonymously.','and friends.','faster.','entirely.','underfoot.','somewhere.','immediately.','today.']
+			*/
+		]
+	})
+	console.log(mySloganizer)
+
+	mySloganizer.summonWheels(5);
+	$sloganizerWrapper.find('.randomizeButton').on('click',function(){
+		mySloganizer.summonWheels(5);
+	})
+		
+});
---
+++ sloganizer.js
@@ -0,0 +1,214 @@
+;(function(global){
+	// UglifyJS define hack.  Used for unit testing.
+	if (typeof SLOGANIZER_NOW === 'undefined') {
+	  SLOGANIZER_NOW = function () {
+	    return +new Date();
+	  };
+	}
+
+	if (typeof SLOGANIZER === 'undefined') {
+	  var global = (function(){return this;})();
+	}
+
+
+	//************
+	//LOGIC HERE
+	var sloganizer= function(options){
+		var defaults = {
+			$el: '',
+			wheelWordCount:6,
+			wordBanks:''
+		};
+		this.settings = $.extend({}, defaults, options);
+		this.wordHeight = 0;
+		this.currentSentence = '';
+		this.wordBankObjs = [];
+
+
+		this.initialize()
+		this.randomizeCurrentWordObjs();
+		
+	};
+	sloganizer.prototype.summonWheels = function(wheelLength){
+		//this.wordBankObjs
+		//this.settings.$el.append($wheel);
+		//console.log(wheelLength)
+		var clones = [];
+		for(var i = 0; i<wheelLength; i++){
+			this.randomizeCurrentWordObjs()
+			for(var j = 0; j<this.wordBankObjs.length; j++){
+				//console.log(this.wordBankObjs[j])
+				//console.log(this.wordBankObjs[j].currentWordObj.word)
+				if(i === wheelLength - 1){
+					this.wordBankObjs[j].$el.append(this.wordBankObjs[j].currentWordObj.$el)
+				} else {
+					var $clone = this.wordBankObjs[j].currentWordObj.$el.clone()
+					this.wordBankObjs[j].$el.append($clone)
+					clones.push($clone);
+				}
+				//this.wordBankObjs[j].$el.append(this.wordBankObjs[j].currentWordObj.$el.clone())
+			}
+			
+			//console.log(this.currentSentence)
+			
+		}
+
+		for(var j = 0; j<this.wordBankObjs.length; j++){
+		}
+
+		var self = this;
+		var callbackCount = 0;
+		for(var j = 0; j<self.wordBankObjs.length; j++){
+			self.settings.$el.append(self.wordBankObjs[j].$el)
+			Tools.dom.cssTransitioner2({
+				target: self.wordBankObjs[j].$el,
+				cssProperty: Tools.dom.translate3dKey,
+				cssValue: {
+					type:'translate3d',
+					values:[0,-(wheelLength - 1)*this.wordHeight,0]
+				},
+				duration: 500,
+				ease: 'ease-out',
+				callback: function($that){
+					console.log('original callback')
+					if(callbackCount === self.wordBankObjs.length - 1){
+						console.log('done!!!',clones.length)
+
+					} else {
+						console.log('not')
+					}
+					callbackCount ++;
+				}
+			});
+		}
+	};
+	sloganizer.prototype.initialize = function(){
+		var wordHeight = 0;
+		
+		for(var i = 0, l = this.settings.wordBanks.length; i<l; i++){
+			var isEndOfSentence = l - i === 1 ? true : false;
+			var $wheel = $('<div class="sloganizerWheel" style="float:left;position:relative;"></div>');
+			this.settings.$el.append($wheel);
+			var widestWord = 0;
+			var wordObjs =[]
+			for(var j = 0, m = this.settings.wordBanks[i].length; j<m; j++){
+				var word = (this.settings.wordBanks[i][j]);
+				var wordClean = word;
+				if(!isEndOfSentence){
+					word += '&nbsp;';
+				}
+				var $word = $('<div class="sloganizerWord" style="float:left;clear:both;margin:0 auto;opacity:0;">'+word+'</div>');
+				$wheel.append($word)
+				if(wordHeight === 0){
+					wordHeight = $word.outerHeight();
+				}
+				if(wordHeight === 0){
+					wordHeight = $word.outerHeight();
+				}
+				this.wordHeight = wordHeight;
+				var wordWidth = $word.outerWidth();
+				if(wordWidth > widestWord){
+					widestWord = wordWidth;
+				}
+				$word.remove();
+				$word.css({
+					'float':'none',
+					'width':wordWidth+'px',
+					'opacity':''
+				})
+				wordObjs.push({
+					$el:$word,
+					word:wordClean,
+					width:wordWidth
+				})
+			}
+			$wheel.css({
+				'width':widestWord+'px'
+				//'height':(this.settings.wordBanks[i].length * wordHeight)+'px',
+			});
+			//console.log(widestWord)
+			this.wordBankObjs.push({
+				$el:$wheel,
+				wordObjs:wordObjs,
+				wordBank:this.settings.wordBanks[i],
+				currentWordObj:null,
+				dyingWordObj:null,
+				currentSentence:null,
+				currentIndex:0,
+				currentWord:this.settings.wordBanks[i][0],
+				dyingIndex:null,
+				dyingWord:null
+			})
+			$wheel.remove()
+			
+			this.settings.$el.css({
+				'height':this.wordHeight + 'px',
+				'overflow':'hidden'
+			});
+			
+		}
+	}
+	sloganizer.prototype.randomizeCurrentWordObjs = function(){
+		var sentence = ''
+		for(var i = 0, l = this.wordBankObjs.length; i<l; i++){
+			this.wordBankObjs[i].dyingWordObj = this.wordBankObjs[i].currentWordObj
+			var wordObjs = this.wordBankObjs[i].wordObjs;
+			this.wordBankObjs[i].dyingIndex = this.wordBankObjs[i].currentIndex;
+			this.wordBankObjs[i].dyingWord = this.wordBankObjs[i].currentWord;
+			if(wordObjs.length === 1){
+
+			} else 
+			if(wordObjs.length <= 2){
+				if(this.wordBankObjs[i].currentIndex === 0){
+					this.wordBankObjs[i].currentIndex = 1;
+				} else {
+					this.wordBankObjs[i].currentIndex = 0;
+				}
+			} else {
+				var randomIndex = Tools.math.returnRandomInt(0,wordObjs.length - 1);
+				if(randomIndex === this.wordBankObjs[i].currentIndex){
+					randomIndex += 1;
+					if(randomIndex > wordObjs.length - 1){
+						randomIndex += -2;
+					}
+				}
+				
+				this.wordBankObjs[i].currentIndex = randomIndex;
+			}
+			var indexedWordObj = wordObjs[this.wordBankObjs[i].currentIndex]
+
+			sentence += indexedWordObj.word;
+
+			this.wordBankObjs[i].currentWordObj = indexedWordObj;
+			
+		}
+		//console.log(sentence)
+	}
+
+
+
+
+
+	var sloganizerInitializer = function(){};
+	sloganizerInitializer.prototype.createSloganizer = function(arguments){
+		return new sloganizer(arguments)
+	}
+
+	var Sloganizer = new sloganizerInitializer();
+	//return objInstance;
+
+	if (typeof exports === 'object') {
+		// nodejs
+		module.exports = Sloganizer;
+	} else if (typeof define === 'function' && define.amd) {
+		// AMD
+		define(function () { return Sloganizer; });
+	} else if (typeof global.Sloganizer === 'undefined') {
+		// Browser: Make `Tweenable` globally accessible.
+		global.Sloganizer = Sloganizer;
+	}
+
+	return Sloganizer;
+
+
+})(this);
