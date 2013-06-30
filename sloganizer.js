992d047e46a83800f42961d97f793c780bc13453
Brian Ephraim <brianephraim@gmail.com>
2013-06-30 10:00:01 -0400

    slaganize button now works

--- sloganizer.js
+++ sloganizer.js
@@ -17,11 +17,11 @@
 		var defaults = {
 			$el: '',
 			wheelWordCount:6,
-			wordBanks:''
+			wordBanks:'',
+			initialSentenceArray: ['Which','cat','is','good?']
 		};
 		this.settings = $.extend({}, defaults, options);
 		this.wordHeight = 0;
-		this.currentSentence = '';
 		this.currentSentenceObj = {
 			sentence: '',
 			$el: $(),
@@ -31,36 +31,67 @@
 		this.clones=$();
 
 		this.initialize()
-		//this.randomizeCurrentWordObjs();
 		
 		
 	};
 	sloganizer.prototype.formulateWheelsAndSentence = function(wheelLength){
 		var self = this;
 		var sentenceArray = [];
-		for(var i = 0; i<wheelLength; i++){
-			//this.randomizeCurrentWordObjs()
+		for(var j = 0; j<this.wordBankObjs.length; j++){
+			this.wordBankObjs[j].$el.children().remove()
+		}
+		for(var i = 0; i<wheelLength; i++){//need to swap this loop with the one below somehow, and integrate the loop above into the mess.
 			for(var j = 0; j<this.wordBankObjs.length; j++){
+				var wordBankObj = self.wordBankObjs[j];
 				this.wordBankObjs[j].wheelLengthXXX = (wheelLength-(Math.round(wheelLength/6)*(this.wordBankObjs.length - j)));
-				if(i === this.wordBankObjs[j].wheelLengthXXX - 1){
-					var $nonClone = this.wordBankObjs[j].currentWordObj.$el
+				if(i === this.wordBankObjs[j].wheelLengthXXX - 1){//For the last iteration, make sure it's not a repeat, and update sentence object
+					if(this.currentSentenceObj.wordBankObjs[j] === wordBankObj.currentSentenceAssignedWordObj){
+						wordBankObj.currentSentenceAssignedWordObj = self.returnRandomWordObj(self.wordBankObjs[j])
+					}
+					var $nonClone = this.wordBankObjs[j].currentSentenceAssignedWordObj.$el
 					this.wordBankObjs[j].$el.append($nonClone)
-					this.wordBankObjs[j].actualCurrentWordObj = this.wordBankObjs[j].currentWordObj;
-					sentenceArray[j] = this.wordBankObjs[j].currentWordObj.word;
-					this.currentSentenceObj.wordBankObjs[j] = this.wordBankObjs[j].currentWordObj.word;
+					sentenceArray[j] = this.wordBankObjs[j].currentSentenceAssignedWordObj.word;
+					this.currentSentenceObj.wordBankObjs[j] = this.wordBankObjs[j].currentSentenceAssignedWordObj;
 				} else {
 					if(i < this.wordBankObjs[j].wheelLengthXXX - 1){
-						var $clone = this.wordBankObjs[j].currentWordObj.$el.clone()
+						var $clone = this.wordBankObjs[j].currentSentenceAssignedWordObj.$el.clone()
 						this.wordBankObjs[j].$el.append($clone)
 						this.clones.push($clone);
-						self.randomizeWordBankObj(self.wordBankObjs[j])
+						wordBankObj.currentSentenceAssignedWordObj = self.returnRandomWordObj(self.wordBankObjs[j])
 					}
 				}
 			}			
 		}
 		var sentenceX = sentenceArray.join('');
-		this.currentSentence = sentenceX;
 		this.currentSentenceObj.sentence = sentenceX;
+	}
+	sloganizer.prototype.returnRandomWordObj = function(wordBankObj){
+		var self = this;
+		var newIndex = 0;
+		var wordObjs = wordBankObj.wordObjs;
+		var currentWordObjIndex = wordBankObj.wordObjs.indexOf(wordBankObj.currentBankAssignedWordObj)
+		if(wordObjs.length === 1){
+
+		} else 
+		if(wordObjs.length === 2){
+			newIndex = currentWordObjIndex ===0 ? 1 : 0;
+		} else {
+			var randomIndex = Tools.math.returnRandomInt(0,wordObjs.length - 1);
+			if(randomIndex === currentWordObjIndex){
+				randomIndex += 1;
+				if(randomIndex > wordObjs.length - 1){
+					randomIndex += -2;
+				}
+			}
+			
+			newIndex = randomIndex;
+		}
+		var indexedWordObj = wordObjs[newIndex]
+		wordBankObj.currentBankAssignedWordObj = indexedWordObj;
+		return indexedWordObj;
+	}
+	sloganizer.prototype.lateralAnimation = function(){
+		var self = this;
 	}
 	sloganizer.prototype.insertWheelsAnimateAndResolveSentence = function(){
 		var self = this;
@@ -91,12 +122,12 @@
 							
 							sentence += self.wordBankObjs[k].currentWord;
 							fullWidth += (self.wordBankObjs[k].widestWordWidth);
-							compactSentenceWidth += self.wordBankObjs[k].actualCurrentWordObj.width;
+							compactSentenceWidth += self.wordBankObjs[k].currentSentenceAssignedWordObj.width;
 							self.wordBankObjs[k].$el.css({'top':'0'})
 							var $tempWheel = self.wordBankObjs[k].$el.clone();
 							$tempWheel.css('width','')
 							$tempWheelContainer.append($tempWheel)
-							self.wordBankObjs[k].horizontalShift = $tempWheel.offset().left - self.wordBankObjs[k].actualCurrentWordObj.$el.offset().left;
+							self.wordBankObjs[k].horizontalShift = $tempWheel.offset().left - self.wordBankObjs[k].currentSentenceAssignedWordObj.$el.offset().left;
 						}
 						var shiftMiddleAdjustValue = (fullWidth - compactSentenceWidth)/2;
 						$tempWheelContainer.remove();
@@ -114,31 +145,47 @@
 								ease: 'ease-out',
 								duration:1000,
 								callback: function($that){
-									console.log('TTTTTTERTRTRT')
 									if(callbackCount2 === n - 1){
-										self.currentSentenceObj.$el = $('<div style="position:absolute;top:0;z-index:9999;width:'+fullWidth+'px;text-align:center;">'+self.currentSentence+'</div>');
+										self.currentSentenceObj.$el = $('<div style="position:absolute;top:0;z-index:9999;width:'+fullWidth+'px;text-align:center;">'+self.currentSentenceObj.sentence+'</div>');
 										self.settings.$el.prepend(self.currentSentenceObj.$el)
 										for(var a = 0, z = self.wordBankObjs.length; a < z; a++){
 											self.wordBankObjs[a].$el.remove();
+											self.wordBankObjs[a].$el.css('left','0');
+
+											reprimeWordObjsArrayForCurrent(self.wordBankObjs[a])
+											function reprimeWordObjsArrayForCurrent(wordBankObj){
+												var index = wordBankObj.wordObjs.indexOf(wordBankObj.currentSentenceAssignedWordObj)
+												var currentTempRemoved = self.wordBankObjs[a].wordObjs.splice(index, 1);
+												wordBankObj.wordObjs.unshift(currentTempRemoved[0])
+												
+											}
+											console.log(self.wordBankObjs[a].wordObjs[0])
+											//SECOND SPIN
+											//SECOND SPIN
+											//SECOND SPIN
+											//SECOND SPIN
+											//SECOND SPIN
+											console.log(self.wordBankObjs[a].currentBankAssignedWordObj,self.wordBankObjs[a].currentSentenceAssignedWordObj)
+											setTimeout(function(){
+												//self.currentSentenceObj.$el.remove()
+												self.formulateWheelsAndSentence(25)
+												//self.insertWheelsAnimateAndResolveSentence()
+											},1000)
+											
+
 										}
 									}
 									callbackCount2++;
 								}
 							});
 						}
-
-
-						
-
 					} else {
 
-						console.log('not',callbackCount)
 					}
 					callbackCount ++;
 				}
 			});
 		}
-
 	}
 	sloganizer.prototype.summonWheels = function(wheelLength){
 		var self = this;
@@ -149,66 +196,72 @@
 
 		
 	};
+	sloganizer.prototype.returnWordObj = function(word,isEndOfSentence,$wheel){
+		var self = this;
+		var wordClean = word;
+		if(!isEndOfSentence){
+			word += '&nbsp;';
+		}
+		var $word = $('<div class="sloganizerWord" style="float:left;clear:both;margin:0 auto;opacity:0;">'+word+'</div>');
+		$wheel.append($word)
+		if(self.wordHeight === 0){
+			self.wordHeight = $word.outerHeight();
+		}
+		if(self.wordHeight === 0){
+			self.wordHeight = $word.outerHeight();
+		}
+		var wordWidth = $word.outerWidth();
+		$word.remove();
+		$word.css({
+			'float':'none',
+			'width':wordWidth+'px',
+			'opacity':''
+		})
+		return {
+			$el:$word,
+			word:word,
+			width:wordWidth
+		}
+
+	}
 	sloganizer.prototype.initialize = function(){
 		var self = this;
 		
-		console.log(self.currentSentenceObj.$el.length)
-		var wordHeight = 0;
+		self.wordHeight = 0;
 		
 		for(var i = 0, l = this.settings.wordBanks.length; i<l; i++){
 			var isEndOfSentence = l - i === 1 ? true : false;
 			var $wheel = $('<div class="sloganizerWheel" style="float:left;position:relative;"></div>');
 			this.settings.$el.append($wheel);
 			var widestWord = 0;
-			var wordObjs =[]
+			var wordObjs =[];
+			var intitialWordObj = self.returnWordObj(self.settings.initialSentenceArray[i],isEndOfSentence,$wheel);
+			widestWord = intitialWordObj.width;
+			wordObjs.push(intitialWordObj)
+
+			this.currentSentenceObj.wordBankObjs[i] = intitialWordObj;
+
 			for(var j = 0, m = this.settings.wordBanks[i].length; j<m; j++){
 				var word = (this.settings.wordBanks[i][j]);
-				var wordClean = word;
-				if(!isEndOfSentence){
-					word += '&nbsp;';
+				var wordObj = self.returnWordObj(word,isEndOfSentence,$wheel);
+				wordObjs.push(wordObj)
+				if(wordObj.width > widestWord){
+					widestWord = wordObj.width;
 				}
-				var $word = $('<div class="sloganizerWord" style="float:left;clear:both;margin:0 auto;opacity:0;">'+word+'</div>');
-				$wheel.append($word)
-				if(wordHeight === 0){
-					wordHeight = $word.outerHeight();
-				}
-				if(wordHeight === 0){
-					wordHeight = $word.outerHeight();
-				}
-				this.wordHeight = wordHeight;
-				var wordWidth = $word.outerWidth();
-				if(wordWidth > widestWord){
-					widestWord = wordWidth;
-				}
-				$word.remove();
-				$word.css({
-					'float':'none',
-					'width':wordWidth+'px',
-					'opacity':''
-				})
-				wordObjs.push({
-					$el:$word,
-					word:word,
-					width:wordWidth
-				})
 			}
 			$wheel.css({
 				'width':widestWord+'px'
 				//'height':(this.settings.wordBanks[i].length * wordHeight)+'px',
 			});
-			console.log(this.settings.wordBanks[0])
 			this.wordBankObjs.push({
 				$el:$wheel,
 				widestWordWidth: widestWord,
 				wordObjs:wordObjs,
 				wordBank:this.settings.wordBanks[i],
-				currentWordObj:wordObjs[0],
+				currentSentenceAssignedWordObj:intitialWordObj,
+				currentBankAssignedWordObj:intitialWordObj,
 				dyingWordObj:null,
-				currentSentence:null,
-				currentIndex:0,
-				currentWord:this.settings.wordBanks[i][0],
-				dyingIndex:null,
-				dyingWord:null
+				currentWord:intitialWordObj.word,
 			})
 			$wheel.remove()
 			
@@ -219,84 +272,20 @@
 			
 		}
 	}
-	sloganizer.prototype.randomizeCurrentWordObjs = function(){
-		var self = this;
-		self.currentSentence = ''
-		for(var i = 0, l = this.wordBankObjs.length; i<l; i++){
-			this.wordBankObjs[i].dyingWordObj = this.wordBankObjs[i].currentWordObj
-			var wordObjs = this.wordBankObjs[i].wordObjs;
-			this.wordBankObjs[i].dyingIndex = this.wordBankObjs[i].currentIndex;
-			this.wordBankObjs[i].dyingWord = this.wordBankObjs[i].currentWord;
-			if(wordObjs.length === 1){
+	
 
-			} else 
-			if(wordObjs.length <= 2){
-				if(this.wordBankObjs[i].currentIndex === 0){
-					this.wordBankObjs[i].currentIndex = 1;
-				} else {
-					this.wordBankObjs[i].currentIndex = 0;
-				}
-			} else {
-				var randomIndex = Tools.math.returnRandomInt(0,wordObjs.length - 1);
-				if(randomIndex === this.wordBankObjs[i].currentIndex){
-					randomIndex += 1;
-					if(randomIndex > wordObjs.length - 1){
-						randomIndex += -2;
-					}
-				}
-				
-				this.wordBankObjs[i].currentIndex = randomIndex;
-			}
-			var indexedWordObj = wordObjs[this.wordBankObjs[i].currentIndex]
-
-			self.currentSentence += indexedWordObj.word;
-			//console.log(self.currentSentence)
-			this.wordBankObjs[i].currentWordObj = indexedWordObj;
-			
-		}
-	}
-
-	sloganizer.prototype.randomizeWordBankObj = function(wordBankObj){
-		var self = this;
-		wordBankObj.dyingWordObj = wordBankObj.currentWordObj
-		var wordObjs = wordBankObj.wordObjs;
-		wordBankObj.dyingIndex = wordBankObj.currentIndex;
-		wordBankObj.dyingWord = wordBankObj.currentWord;
-		if(wordObjs.length === 1){
-
-		} else 
-		if(wordObjs.length <= 2){
-			if(wordBankObj.currentIndex === 0){
-				wordBankObj.currentIndex = 1;
-			} else {
-				wordBankObj.currentIndex = 0;
-			}
-		} else {
-			var randomIndex = Tools.math.returnRandomInt(0,wordObjs.length - 1);
-			if(randomIndex === wordBankObj.currentIndex){
-				randomIndex += 1;
-				if(randomIndex > wordObjs.length - 1){
-					randomIndex += -2;
-				}
-			}
-			
-			wordBankObj.currentIndex = randomIndex;
-		}
-		var indexedWordObj = wordObjs[wordBankObj.currentIndex]
-		wordBankObj.currentWordObj = indexedWordObj;
-		console.log(indexedWordObj.word)
-	}
+	
 
 
 
 
 
-	var sloganizerInitializer = function(){};
-	sloganizerInitializer.prototype.createSloganizer = function(arguments){
+	var sloganizerBootstraper = function(){};
+	sloganizerBootstraper.prototype.createSloganizer = function(arguments){
 		return new sloganizer(arguments)
 	}
 
-	var Sloganizer = new sloganizerInitializer();
+	var Sloganizer = new sloganizerBootstraper();
 	//return objInstance;
 
 	if (typeof exports === 'object') {
