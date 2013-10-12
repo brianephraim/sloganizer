696caec3ef49bdf54d3df15a522fd1a757f37d77
Brian Ephraim <brianephraim@gmail.com>
2013-10-12 13:10:01 -0400

    r.js built

--- main2.js
+++ main2.js
@@ -12,7 +12,7 @@ require.config({
         }
     }
 });
-require(['module2'], function(){
+require(['asdf/module2'], function(){
 	console.log('asdf')
 	require(['module2'], function(x){//THIS IS THE SECOND TIME module2 HAS BEEN REQUIRED, BUT ITS BOILERPLATE ONLY FIRE'S ONCE.  IT'S EFFICIENT.
 		console.log(new x().getModule1Name())
